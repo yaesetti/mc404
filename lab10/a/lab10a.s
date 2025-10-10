9842ce4789aaa8bb113ade1891e37d92639d2181
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-10-10 08:31:42 -0300

    finished lab10a first try :)

---
+++ lab10/a/lab10a.s
@@ -0,0 +1,242 @@
+.bss
+    temp_buffer: .skip 1000
+
+.text
+strlen:
+    # a0 has the string pointer
+    mv t0, a0                   # copy of the string pointer
+    li t1, 0                    # string size
+    1:
+        lb t2, 0(t0)            # current char
+
+        beqz t2, finished_len   # checks if the char is '\0', if it is, return
+
+        addi t1, t1, 1          # if not, increment the string size
+        addi t0, t0, 1          # points to the next char
+        j 1b                    # loop again
+    1:
+    
+    finished_len:
+        mv a0, t1               # moves the string size to a0
+        ret                     # returns
+
+.globl puts
+puts:
+    # a0 has the string pointer
+    addi sp, sp, -16
+    sw a0, 8(sp)
+    sw ra, 4(sp)
+    sw fp, 0(sp)
+    addi fp, sp, 16
+    jal strlen                  # get string length
+    mv t0, a0                   # move
+
+    lw a0, 8(sp)                # restores a0
+    add a0, a0, t0              # goes to position '\0'
+    li t1, '\n'                 # load '\n'
+    sb t1, 0(a0)                # changes '\0' for '\n'
+    addi t0, t0, 1              # increments string size for '\n'
+
+    mv t1, a0                   # points to the '\n'
+
+    li a0, 1                    # file descriptor 1 (stdout)
+    lw a1, 8(sp)                # load string
+    mv a2, t0                   # string size
+    li a7, 64                   # syscall write (64)
+    ecall
+
+    sb zero, 0(t1)              # changes '\n' back to '\0'
+
+    lw fp, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 16
+    mv a0, t0
+    ret
+
+.globl gets
+gets:
+    # a0 has the buffer pointer
+    mv t0, a0                   # t0 has the buffer pointer
+    mv t6, t0                   # creates a copy of the buffer
+
+    1:
+        li a0, 0                # file descriptor 0 (stdin)
+        mv a1, t0               # load buffer
+        li a2, 1                # size 1
+        li a7, 63               # syscall read (63)
+        ecall
+
+        lb t1, 0(t0)            # loads the current char
+        li t2, '\n'             # loads '\n'
+        
+        beq t1, t2, 1f          # if current character is '\n'
+
+        addi t0, t0, 1          # else, increment pointer
+        j 1b                    # go back to the loop
+    1:
+    sw zero, 0(t0)              # changes the '\n' to '\0'
+    mv a0, t6
+    ret
+
+.globl atoi
+atoi:
+    # a0 has the string pointer
+    mv t0, a0                   # t0 has the string pointer
+
+    1:                          # loop that skips whitespace
+        lb t1, 0(t0)
+        li t2, ' '
+        
+        bgt t1, t2, 1f
+        addi t0, t0, 1
+        j 1b
+    1:
+    
+    lb t1, 0(t0)
+
+    li t6, 0                    # negative flag set to false
+    li t2, '-'
+    bne t1, t2, 2f              # if not '-' skips
+    li t6, 1                    # else, negative flag set to true
+    addi t0, t0, 1              # increments the string pointer
+    j 1f
+
+    2:
+    li t2, '9'
+    bgt t1, t2, not_number
+
+    1:
+
+    li a0, 0                    # int to be returned
+
+    1:
+        lb t1, 0(t0)
+        li t2, '0'
+        blt t1, t2, atoi_done
+        li t2, '9'
+        bgt t1, t2, atoi_done
+
+        addi t1, t1, -'0'
+
+        li t2, 10
+
+        mul a0, a0, t2
+        add a0, a0, t1
+
+        addi t0, t0, 1
+        j 1b
+    1:
+
+    not_number:
+        li a0, 0
+        ret
+    
+    atoi_done:
+        beqz t6, 1f
+        sub a0, zero, a0
+        1:
+        ret
+
+.globl itoa
+itoa:
+    # a0 has the int value
+    # a1 has the buffer pointer
+    # a2 has the base
+    mv t0, a1                   # t0 has a copy of the buffer pointer
+
+    bnez a0, 1f
+    li t0, '0'
+    sb t0, 0(a1)
+    sb zero, 1(a1)
+    mv a0, a1
+    ret
+
+    1:
+    li t1, 10
+    beq a2, t1, dec_base
+
+    la t1, temp_buffer          # will store the inverted number
+
+    li t6, 0                    # counter
+    2:
+    1:
+        beqz a0, 1f             # stops when a0 == 0
+        remu t3, a0, a2          # gets the first digit
+
+        divu a0, a0, a2          # removes the first digit
+        
+        li t4, 9
+        ble t3, t4, 4f          # if <= 9 goto 4:
+        addi t3, t3, 87         # converts to a-f ASCII
+        j 3f
+
+        4:
+        addi t3, t3, '0'        # converts to 0-9 ASCII
+
+        3:
+        sb t3, 0(t1)            # stores in the temp_buffer
+        addi t1, t1, 1          # increments the temp_buffer pointer
+        addi t6, t6, 1          # increments the counter
+        j 1b                    # loops
+    1:
+
+    addi t1, t1, -1
+    1:
+        beqz t6, 1f
+        lb t2, 0(t1)
+        sb t2, 0(t0)
+
+        addi t1, t1, -1
+        addi t0, t0, 1
+        addi t6, t6, -1
+        j 1b
+    1:
+
+    sb zero, 0(t0)              # adds the null terminator
+
+    mv a0, a1
+    ret
+
+    dec_base:
+        bgez a0, 2b             # if a0 >= 0, skips to loop
+                                # else
+        li t1, '-'              # load '-'
+        sb t1, 0(t0)            # stores at the start of the buffer
+        addi t0, t0, 1          # increments the buffer pointer
+        sub a0, zero, a0        # makes the number positive
+        j 2b                    # goes to the loop
+
+.globl linked_list_search
+linked_list_search:
+    # a0 has the head node pointer
+    # a1 has the value we are searching
+    li t0, 0                    # index counter
+
+    1:
+        beqz a0, not_found
+
+        lw t1, 0(a0)            # first number
+        lw t2, 4(a0)            # second number
+
+        add t1, t1, t2          # sum of the numbers
+
+        beq a1, t1, found
+
+        addi t0, t0, 1          # increments the index counter
+        lw a0, 8(a0)            # next node become the current node
+        j 1b
+    1:
+
+    found:
+        mv a0, t0               # returns the index
+        ret
+
+    not_found:
+        li a0, -1               # returns -1 if not found
+        ret
+
+.globl exit
+exit:
+    li a0, 0
+    li a7, 93
+    ecall
---
+++ lab10/a/testing.c
@@ -0,0 +1,192 @@
+
+    #include "lib.h"
+
+    char buffer[100];
+    int number = 1311;
+
+    #define NULL 0
+
+    void run_operation(int op){
+        int val;
+        Node node_99,node_90,node_75,node_133,node_66,node_17,node_79,node_116,node_109,node_98,node_53,node_138,node_82,node_100,node_93,node_7,node_115,node_14,node_105,node_45,node_89,node_122,node_132,node_125,node_41,node_112,node_137,node_50,node_80,node_84,node_63,node_57,node_91,node_73,node_135,node_120,node_44,node_37,node_13,node_8,node_128,node_20,node_119,node_26,node_28,node_88,head_node,node_25,node_110,node_118,node_33,node_54,node_124,node_87,node_95,node_123,node_61,node_96,node_59,node_70,node_10,node_34,node_67,node_102,node_36,node_11,node_60,node_136,node_114,node_58,node_71,node_39,node_104,node_46,node_22,node_29,node_15,node_74,node_134,node_31,node_47,node_42,node_117,node_86,node_68,node_56,node_30,node_101,node_21,node_113,node_52,node_1,node_12,node_62,node_65,node_18,node_40,node_69,node_51,node_108,node_111,node_76,node_38,node_81,node_106,node_103,node_23,node_85,node_77,node_130,node_97,node_27,node_126,node_55,node_107,node_24,node_5,node_9,node_72,node_3,node_19,node_35,node_48,node_94,node_121,node_129,node_49,node_92,node_83,node_43,node_127,node_6,node_16,node_2,node_4,node_131,node_78,node_64,node_32;
+        head_node.val1 = 10;head_node.val2 = -4;head_node.next = &node_1;
+        node_1.val1 = 56;node_1.val2 = 78;node_1.next = &node_2;
+        node_2.val1 = -654;node_2.val2 = 590;node_2.next = &node_3;
+        node_3.val1 = -100;node_3.val2 = -43;node_3.next = &node_4;
+        node_4.val1 = -8382;node_4.val2 = 1203;node_4.next = &node_5;
+        node_5.val1 = 4946;node_5.val2 = 1428;node_5.next = &node_6;
+        node_6.val1 = 2373;node_6.val2 = -8420;node_6.next = &node_7;
+        node_7.val1 = 4101;node_7.val2 = -679;node_7.next = &node_8;
+        node_8.val1 = -6296;node_8.val2 = 320;node_8.next = &node_9;
+        node_9.val1 = 8672;node_9.val2 = -15995;node_9.next = &node_10;
+        node_10.val1 = -9496;node_10.val2 = 1631;node_10.next = &node_11;
+        node_11.val1 = 3666;node_11.val2 = 28;node_11.next = &node_12;
+        node_12.val1 = 9696;node_12.val2 = -1022;node_12.next = &node_13;
+        node_13.val1 = 4341;node_13.val2 = -14103;node_13.next = &node_14;
+        node_14.val1 = 6855;node_14.val2 = -14900;node_14.next = &node_15;
+        node_15.val1 = -7679;node_15.val2 = 9651;node_15.next = &node_16;
+        node_16.val1 = -5603;node_16.val2 = 6526;node_16.next = &node_17;
+        node_17.val1 = -4824;node_17.val2 = -1377;node_17.next = &node_18;
+        node_18.val1 = 6474;node_18.val2 = -8579;node_18.next = &node_19;
+        node_19.val1 = -6964;node_19.val2 = 16910;node_19.next = &node_20;
+        node_20.val1 = 9764;node_20.val2 = -19122;node_20.next = &node_21;
+        node_21.val1 = -8410;node_21.val2 = -813;node_21.next = &node_22;
+        node_22.val1 = 4310;node_22.val2 = -2560;node_22.next = &node_23;
+        node_23.val1 = 6803;node_23.val2 = -11100;node_23.next = &node_24;
+        node_24.val1 = 7936;node_24.val2 = -1595;node_24.next = &node_25;
+        node_25.val1 = -4629;node_25.val2 = 9103;node_25.next = &node_26;
+        node_26.val1 = 3583;node_26.val2 = -3526;node_26.next = &node_27;
+        node_27.val1 = 2132;node_27.val2 = 2642;node_27.next = &node_28;
+        node_28.val1 = -9936;node_28.val2 = 2427;node_28.next = &node_29;
+        node_29.val1 = 9858;node_29.val2 = -3583;node_29.next = &node_30;
+        node_30.val1 = 1438;node_30.val2 = -4294;node_30.next = &node_31;
+        node_31.val1 = -2650;node_31.val2 = -2519;node_31.next = &node_32;
+        node_32.val1 = 6887;node_32.val2 = 384;node_32.next = &node_33;
+        node_33.val1 = -7302;node_33.val2 = 16733;node_33.next = &node_34;
+        node_34.val1 = -6053;node_34.val2 = 1064;node_34.next = &node_35;
+        node_35.val1 = 3009;node_35.val2 = 2734;node_35.next = &node_36;
+        node_36.val1 = -1284;node_36.val2 = -8306;node_36.next = &node_37;
+        node_37.val1 = -6578;node_37.val2 = -252;node_37.next = &node_38;
+        node_38.val1 = -5854;node_38.val2 = 10314;node_38.next = &node_39;
+        node_39.val1 = 3019;node_39.val2 = -12390;node_39.next = &node_40;
+        node_40.val1 = -3689;node_40.val2 = 5021;node_40.next = &node_41;
+        node_41.val1 = 2195;node_41.val2 = 7352;node_41.next = &node_42;
+        node_42.val1 = 2702;node_42.val2 = -12383;node_42.next = &node_43;
+        node_43.val1 = 429;node_43.val2 = 3068;node_43.next = &node_44;
+        node_44.val1 = 5246;node_44.val2 = -936;node_44.next = &node_45;
+        node_45.val1 = -7860;node_45.val2 = 344;node_45.next = &node_46;
+        node_46.val1 = -1250;node_46.val2 = 7424;node_46.next = &node_47;
+        node_47.val1 = -4862;node_47.val2 = 5944;node_47.next = &node_48;
+        node_48.val1 = 163;node_48.val2 = 8;node_48.next = &node_49;
+        node_49.val1 = -2220;node_49.val2 = 370;node_49.next = &node_50;
+        node_50.val1 = -3405;node_50.val2 = 9895;node_50.next = &node_51;
+        node_51.val1 = 9572;node_51.val2 = -16861;node_51.next = &node_52;
+        node_52.val1 = -9419;node_52.val2 = -203;node_52.next = &node_53;
+        node_53.val1 = -6450;node_53.val2 = 4559;node_53.next = &node_54;
+        node_54.val1 = -2394;node_54.val2 = 7933;node_54.next = &node_55;
+        node_55.val1 = -3342;node_55.val2 = 828;node_55.next = &node_56;
+        node_56.val1 = -2663;node_56.val2 = 6484;node_56.next = &node_57;
+        node_57.val1 = 9889;node_57.val2 = -4218;node_57.next = &node_58;
+        node_58.val1 = 7586;node_58.val2 = -15416;node_58.next = &node_59;
+        node_59.val1 = -9524;node_59.val2 = 12177;node_59.next = &node_60;
+        node_60.val1 = -8383;node_60.val2 = 14419;node_60.next = &node_61;
+        node_61.val1 = 6510;node_61.val2 = -9564;node_61.next = &node_62;
+        node_62.val1 = -7690;node_62.val2 = 17116;node_62.next = &node_63;
+        node_63.val1 = -9675;node_63.val2 = 16521;node_63.next = &node_64;
+        node_64.val1 = -5826;node_64.val2 = -4163;node_64.next = &node_65;
+        node_65.val1 = 624;node_65.val2 = 2447;node_65.next = &node_66;
+        node_66.val1 = -5532;node_66.val2 = 6830;node_66.next = &node_67;
+        node_67.val1 = -1314;node_67.val2 = 6220;node_67.next = &node_68;
+        node_68.val1 = 5093;node_68.val2 = 261;node_68.next = &node_69;
+        node_69.val1 = -1858;node_69.val2 = 5548;node_69.next = &node_70;
+        node_70.val1 = -7113;node_70.val2 = 15087;node_70.next = &node_71;
+        node_71.val1 = -8928;node_71.val2 = 4391;node_71.next = &node_72;
+        node_72.val1 = 6033;node_72.val2 = -9727;node_72.next = &node_73;
+        node_73.val1 = 1031;node_73.val2 = -6880;node_73.next = &node_74;
+        node_74.val1 = 5687;node_74.val2 = 326;node_74.next = &node_75;
+        node_75.val1 = -3874;node_75.val2 = -1542;node_75.next = &node_76;
+        node_76.val1 = 6916;node_76.val2 = 2295;node_76.next = &node_77;
+        node_77.val1 = -5136;node_77.val2 = 3019;node_77.next = &node_78;
+        node_78.val1 = -1010;node_78.val2 = -7428;node_78.next = &node_79;
+        node_79.val1 = 8005;node_79.val2 = -8407;node_79.next = &node_80;
+        node_80.val1 = 7647;node_80.val2 = -5953;node_80.next = &node_81;
+        node_81.val1 = 1689;node_81.val2 = -1026;node_81.next = &node_82;
+        node_82.val1 = 440;node_82.val2 = -9139;node_82.next = &node_83;
+        node_83.val1 = -7363;node_83.val2 = 4616;node_83.next = &node_84;
+        node_84.val1 = -982;node_84.val2 = 2043;node_84.next = &node_85;
+        node_85.val1 = 8074;node_85.val2 = -2537;node_85.next = &node_86;
+        node_86.val1 = 7225;node_86.val2 = 990;node_86.next = &node_87;
+        node_87.val1 = 6509;node_87.val2 = -14440;node_87.next = &node_88;
+        node_88.val1 = 9729;node_88.val2 = -16929;node_88.next = &node_89;
+        node_89.val1 = 7044;node_89.val2 = -1808;node_89.next = &node_90;
+        node_90.val1 = 7262;node_90.val2 = 277;node_90.next = &node_91;
+        node_91.val1 = -1137;node_91.val2 = 9139;node_91.next = &node_92;
+        node_92.val1 = -8219;node_92.val2 = 2131;node_92.next = &node_93;
+        node_93.val1 = -8654;node_93.val2 = 10473;node_93.next = &node_94;
+        node_94.val1 = 4578;node_94.val2 = -14110;node_94.next = &node_95;
+        node_95.val1 = -477;node_95.val2 = 8206;node_95.next = &node_96;
+        node_96.val1 = -3441;node_96.val2 = 13177;node_96.next = &node_97;
+        node_97.val1 = -4365;node_97.val2 = 11821;node_97.next = &node_98;
+        node_98.val1 = -7096;node_98.val2 = 5216;node_98.next = &node_99;
+        node_99.val1 = 5709;node_99.val2 = 2323;node_99.next = &node_100;
+        node_100.val1 = -7666;node_100.val2 = 5398;node_100.next = &node_101;
+        node_101.val1 = -4729;node_101.val2 = 441;node_101.next = &node_102;
+        node_102.val1 = 9890;node_102.val2 = -13955;node_102.next = &node_103;
+        node_103.val1 = -7721;node_103.val2 = 2453;node_103.next = &node_104;
+        node_104.val1 = -7606;node_104.val2 = 1239;node_104.next = &node_105;
+        node_105.val1 = -7497;node_105.val2 = 6205;node_105.next = &node_106;
+        node_106.val1 = 7086;node_106.val2 = -14187;node_106.next = &node_107;
+        node_107.val1 = -2761;node_107.val2 = 9051;node_107.next = &node_108;
+        node_108.val1 = -3288;node_108.val2 = 6974;node_108.next = &node_109;
+        node_109.val1 = -2909;node_109.val2 = -1761;node_109.next = &node_110;
+        node_110.val1 = 6712;node_110.val2 = -12442;node_110.next = &node_111;
+        node_111.val1 = -7671;node_111.val2 = 5584;node_111.next = &node_112;
+        node_112.val1 = -6913;node_112.val2 = -1757;node_112.next = &node_113;
+        node_113.val1 = 1607;node_113.val2 = 8088;node_113.next = &node_114;
+        node_114.val1 = -1814;node_114.val2 = 5039;node_114.next = &node_115;
+        node_115.val1 = -1017;node_115.val2 = 10454;node_115.next = &node_116;
+        node_116.val1 = 3840;node_116.val2 = -2951;node_116.next = &node_117;
+        node_117.val1 = 7660;node_117.val2 = 60;node_117.next = &node_118;
+        node_118.val1 = 744;node_118.val2 = -4832;node_118.next = &node_119;
+        node_119.val1 = 1041;node_119.val2 = -566;node_119.next = &node_120;
+        node_120.val1 = 4946;node_120.val2 = -4431;node_120.next = &node_121;
+        node_121.val1 = -5151;node_121.val2 = 658;node_121.next = &node_122;
+        node_122.val1 = -2230;node_122.val2 = 4467;node_122.next = &node_123;
+        node_123.val1 = -8311;node_123.val2 = 9215;node_123.next = &node_124;
+        node_124.val1 = -5562;node_124.val2 = 848;node_124.next = &node_125;
+        node_125.val1 = -6882;node_125.val2 = 10064;node_125.next = &node_126;
+        node_126.val1 = 5048;node_126.val2 = -2564;node_126.next = &node_127;
+        node_127.val1 = 1352;node_127.val2 = -489;node_127.next = &node_128;
+        node_128.val1 = -7378;node_128.val2 = 14417;node_128.next = &node_129;
+        node_129.val1 = -8506;node_129.val2 = 12414;node_129.next = &node_130;
+        node_130.val1 = 5306;node_130.val2 = 827;node_130.next = &node_131;
+        node_131.val1 = -6163;node_131.val2 = 2378;node_131.next = &node_132;
+        node_132.val1 = -8331;node_132.val2 = 5131;node_132.next = &node_133;
+        node_133.val1 = 9860;node_133.val2 = -1233;node_133.next = &node_134;
+        node_134.val1 = 9694;node_134.val2 = -3281;node_134.next = &node_135;
+        node_135.val1 = -8622;node_135.val2 = 6057;node_135.next = &node_136;
+        node_136.val1 = -7693;node_136.val2 = 866;node_136.next = &node_137;
+        node_137.val1 = 7786;node_137.val2 = -16438;node_137.next = &node_138;
+        node_138.val1 = -4645;node_138.val2 = -4698;node_138.next = NULL;
+        
+        switch (op){
+            case 0:
+                puts(buffer);
+                break;
+
+            case 1:
+                gets(buffer);
+                puts(buffer);
+                break;
+
+            case 2:
+                puts(itoa(number, buffer, 10));
+                break;
+
+            case 3:
+                puts(itoa(atoi(gets(buffer)), buffer, 16));
+                break;
+
+            case 4:
+                gets(buffer);
+                puts(buffer);
+                gets(buffer);
+                puts(buffer);
+                break;
+
+            case 5:
+                val = atoi(gets(buffer));
+                puts(itoa(linked_list_search(&head_node, val), buffer, 10));
+                break;
+            
+            default:
+                break;
+            }
+    }
+
+    void _start(){
+        int operation = atoi(gets(buffer));
+        run_operation(operation);
+        exit(0);
+    }
+    
