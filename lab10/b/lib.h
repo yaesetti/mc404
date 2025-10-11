78cdd774352c98e9df306b55bd42eea8352c4262
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-10-11 09:03:26 -0300

    finished lab10b first try too :D

---
+++ lab10/b/lib.h
@@ -0,0 +1,11 @@
+typedef struct Node {
+    int val;
+    struct Node *left, *right;
+} Node;
+
+int recursive_tree_search(Node *root_node, int val);
+void puts ( const char *str );
+char *gets ( char *str );
+int atoi (const char *str);
+char *itoa ( int value, char *str, int base );
+void exit(int code);
